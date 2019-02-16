dcb1de56df9581bec52a4553ab932250eac43e1e
ReddishYeti7847 <noriyasu200081@gmail.com>
2019-02-16 15:30:32 +0900

    四角を描画した。

--- app/src/main/java/com/example/user/canvasapp01/CanvasView.java
+++ app/src/main/java/com/example/user/canvasapp01/CanvasView.java
@@ -2,6 +2,7 @@ package com.example.user.canvasapp01;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.View;
 
@@ -16,7 +17,24 @@ public class CanvasView extends View {
 
     @Override
     protected void onDraw(Canvas canvas){
-        canvas.drawCircle(200,400,50,paint);
+        //キャンバスの大きさを調べる
+        float sx = canvas.getWidth();
+        float sy = canvas.getHeight();
+
+        //背景を塗りつぶす
+        canvas.drawColor(Color.BLACK);
+
+        //円を描く
+        paint.setColor(Color.rgb(0,255,160));
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(10);
+
+        canvas.drawCircle(sx / 2,sy / 2,200, paint);
+
+        //四角を描く
+        paint.setColor(Color.rgb(192, 255, 0));
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawRect(50,20,250,100, paint);
     }
 
 
